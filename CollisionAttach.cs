e30645c590ebab8b01e8b4525ca9f0f19e2d3c6a
YuSun <yshyjn@gmail.com>
2016-11-28 11:10:56 +0900

    first version

---
+++ CollisionAttach.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionAttach : MonoBehaviour {
+
+	public bool connect = false;
+    public bool jack_cir = false;
+
+	Vector3 newPosition;
+	Vector3 newRotation;
+
+	// Use this for initialization
+	void Start () {
+
+		newPosition = new Vector3 (-3.5f, 0.8f, -101.5f);
+		newRotation = new Vector3 (270, 30, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+		Vector3 v;//Vector3 변수를 선언  
+        v = transform.position;
+        if ((transform.position.x >= -4.0f && transform.position.x <= -3.0f) && (transform.position.y >= 0.0f && transform.position.y <= 1.0f) && (transform.position.z >= -102.0f && transform.position.z <= -100.0f))
+        {
+            connect = true;
+        }
+            //해당 오브젝트의 포지션값을 v에 넣고
+        if(connect == true)
+        {
+            jack_cir = true;
+			transform.position = newPosition;
+			transform.rotation = Quaternion.Euler(newRotation);
+            this.GetComponent<Rigidbody>().isKinematic = true;
+           GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        }
+        
+	
+	}
+
+
+	
+}
---
+++ CollisionSteer.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionSteer : MonoBehaviour
+{
+	public bool connect;
+    public bool steer_cir;
+    GameObject jacks;
+    GameObject steeringwheels;
+
+	Vector3 newPosition;
+	Vector3 newRotation;
+	Vector3 newScale;
+    
+    int j;
+	public GameObject ssmall;
+
+    // Use this for initialization
+    void Start()
+    {
+		connect = false;
+		steer_cir = false;
+
+		j = 0;
+		ssmall = GameObject.Find("쪼꼬");
+
+		newPosition = new Vector3(-7.15f, 1.96f, -0.38f);
+		newRotation = new Vector3(80.9f, 77.61f, 225.45f);
+		newScale = new Vector3(0, 0, 0.001f);
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (connect)
+        {
+            steer_cir = true;
+           
+			transform.localPosition = newPosition;
+			transform.rotation = Quaternion.Euler(newRotation);
+            
+           GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+           this.transform.parent = ssmall.transform;
+           this.GetComponent<Rigidbody>().isKinematic = true;
+           Destroy(this.GetComponent<BoxCollider>());
+        }
+    }
+
+   public  void Scale()
+    {
+		transform.localScale -= newScale;
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "CS")
+        {
+            connect = true;
+        }
+    }
+}
